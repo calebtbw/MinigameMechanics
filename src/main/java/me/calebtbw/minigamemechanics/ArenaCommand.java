ac28e15ddbedd077264effafa15e06e1843e0a64
Caleb T <74537247+calebtbw@users.noreply.github.com>
2021-02-03 17:10:05 +0800

    Update ArenaCommand.java

--- src/main/java/me/calebtbw/minigamemechanics/ArenaCommand.java
+++ src/main/java/me/calebtbw/minigamemechanics/ArenaCommand.java
@@ -7,7 +7,6 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import java.util.logging.Logger;
 
 public class ArenaCommand implements CommandExecutor {
 
